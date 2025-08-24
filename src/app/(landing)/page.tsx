b444bc47001045539c0247c0bc8c825b85e5f1a0
Prateek Kumar <prateek.kumar.original@gmail.com>
2025-08-24 20:07:02 +0530

    debug

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,6 +3,7 @@ import { Heading } from "./_components/Heading";
 import { Heroes } from "./_components/Heroes";
 
 const LandingPage = () => {
+  console.log(process.env.CLERK_JWT_ISSUER_DOMAIN);
   return (
     <div className='min-h-full flex flex-col dark:bg-[#1F1F1F]'>
       <div className='flex flex-col items-center justify-center md:justify-start text-center gap-y-16 flex-1 px-6 pb-10'>
